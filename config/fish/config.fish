e489ffdea0ca21ec7ca275ddd7431be5b94b32e7
Adam Pash <adam.pash@gmail.com>
2015-10-29 12:22:21 -0400

    Updated paths and added rvm function

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,6 @@
 fish_vi_mode
 fish_vi_cursor
+
+set PATH $PATH /Users/aop/.rvm/gems/ruby-2.2.0/bin /Users/aop/.rvm/gems/ruby-2.2.0@global/bin /Users/aop/.rvm/rubies/ruby-2.2.0/bin /usr/local/nvm/versions/node/v4.2.1/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/MacGPG2/bin /Users/aop/.rvm/bin /usr/bin /Users/aop/.rvm/bin
+
+rvm default
