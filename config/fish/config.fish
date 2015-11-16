bf9b1501ad75967395fadd8adb9a2a300d6e4ff2
Adam Pash <adam.pash@gmail.com>
2015-11-16 10:25:32 -0500

    Updated autocomplete colors

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,7 @@ fish_vi_mode
 fish_vi_cursor
 
 . "$HOME/.config/fish/functions/aliases.fish"
+. "$HOME/.config/fish/functions/git.fish"
 
 set PATH $PATH /Users/aop/.rvm/gems/ruby-2.2.0/bin /Users/aop/.rvm/gems/ruby-2.2.0@global/bin /Users/aop/.rvm/rubies/ruby-2.2.0/bin /usr/local/nvm/versions/node/v4.2.1/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/MacGPG2/bin /Users/aop/.rvm/bin /usr/bin /Users/aop/.rvm/bin /Applications/Postgres.app/Contents/Versions/9.4/bin
 
