acc47481b331dae2624dde1454b5ad217ba045a0
Adam Pash <adam.pash@gmail.com>
2015-11-16 13:01:37 -0500

    added interactive starter

--- config/fish/functions/aliases.fish
+++ config/fish/functions/aliases.fish
@@ -6,3 +6,17 @@ alias db "cd ~/Dropbox"
 alias labs "cd ~/code/labs"
 alias code "cd ~/code"
 
+function ls
+  command ls -lahG $argv
+end
+
+
+function new_interactive
+  git clone git@github.com:adampash/react-redux-static-boilerplate.git
+  mv react-redux-static-boilerplate $argv
+  cd $argv
+  rm -rf .git
+  git init
+  git add .
+  git commit -m "initial commit"
+end
