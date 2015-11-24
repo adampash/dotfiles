6a6a63f04535fd73645425ff5257921174fd6b0d
Adam Pash <adam.pash@gmail.com>
2015-11-24 14:34:45 -0500

    improved gh function

--- config/fish/functions/git.fish
+++ config/fish/functions/git.fish
@@ -22,12 +22,21 @@ end
 
 function gh
   if test -d .git
-    echo "Opening github repo"
-    open (git remote -v | grep origin | grep fetch | awk '{print $2}' | sed 's/git@/http:\/\//' | sed 's/com:/com\//' | head -n1)
+    # set remote (git remote -v)
+    if git remote -v | grep github.com
+      echo "Opening github repo"
+      open (git remote -v | grep origin | grep fetch | awk '{print $2}' | sed 's/git@/http:\/\//' | sed 's/com:/com\//' | head -n1)
+    else
+      new_github
+    end
   else
-    echo "Creating github repo"
+    echo "Initializing git repository"
     git init
-    hub create
+    new_github
   end
 end
 
+function new_github
+  echo "Creating github repo"
+  hub create
+end
